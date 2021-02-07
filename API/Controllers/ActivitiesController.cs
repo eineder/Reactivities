9896042682b65017e016606680216915efae8b61
Max Eineder <max.eineder@googlemail.com>
2021-02-07 18:09:25 +0100

    Add details query and handler

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,4 +1,5 @@
 
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Application.Activities;
@@ -19,9 +20,16 @@ namespace API
             this._mediator = mediator;
         }
 
+        [HttpGet]
         public async Task<ActionResult<List<Activity>>> List()
         {
             return await _mediator.Send(new List.Query());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> Details(Guid id)
+        {
+            return await _mediator.Send(new Details.Query { Id = id });
         }
 
     }
