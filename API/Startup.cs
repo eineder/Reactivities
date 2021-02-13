1c4d497d97035467b3af0677431cd5b82bef0c12
Max Eineder <max.eineder@googlemail.com>
2021-02-13 13:49:58 +0100

    Migrate to .Net 5.0

--- API/Startup.cs
+++ API/Startup.cs
@@ -43,6 +43,7 @@ namespace API
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
+                c.CustomSchemaIds(type => type.ToString());
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
         }
