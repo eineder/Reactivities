bbf98b092dfd49f8867a39e5a2616f51097f8c08
Maximilian Eineder <max.eineder@googlemail.com>
2021-03-11 20:27:23 +0100

    Only load activities once to avoid unnecessary IO.

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,15 +1,16 @@
-import { observer } from 'mobx-react-lite';
-import { useEffect } from 'react';
-import { Grid, GridColumn } from 'semantic-ui-react';
-import LoadingComponent from '../../../app/layout/LoadingComponent';
-import { useStore } from '../../../app/stores/Store';
-import ActivityList from './ActivityList';
+import { observer } from "mobx-react-lite";
+import { useEffect } from "react";
+import { Grid, GridColumn } from "semantic-ui-react";
+import LoadingComponent from "../../../app/layout/LoadingComponent";
+import { useStore } from "../../../app/stores/Store";
+import ActivityList from "./ActivityList";
 
 export default observer(function ActivityDashboard() {
   const { activityStore } = useStore();
+  const { loadActivities, activityRegistry } = activityStore;
 
   useEffect(() => {
-    activityStore.loadActivities();
+    if (activityRegistry.size === 0) loadActivities();
   }, [activityStore]);
 
   if (activityStore.loadingInitial) {
