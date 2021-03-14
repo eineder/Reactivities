0d5c66b5e6582589884c2df714187acef0314042
Maximilian Eineder <max.eineder@googlemail.com>
2021-03-14 22:56:09 +0100

    Style home page

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -1,14 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Container } from "semantic-ui-react";
+import { Image, Container, Header, Segment, Button } from "semantic-ui-react";
 
 export default function HomePage() {
   return (
-    <Container style={{ marginTop: "em" }}>
-      <h1>Home Page</h1>
-      <h3>
-        Go to <Link to="/activities">Activities</Link>
-      </h3>
-    </Container>
+    <Segment inverted textAlign="center" vertical className="masthead">
+      <Container>
+        <Header as="h1" inverted>
+          <Image
+            size="massive"
+            src="/assets/logo.png"
+            alt="logo"
+            style={{ marginBottom: 12 }}
+          />
+          Reactivities
+        </Header>
+        <Header inverted as="h2" content="Welcome to Reactivities"></Header>
+        <Button as={Link} to="/activities" size="huge">
+          Take me to the Activities!
+        </Button>
+      </Container>
+    </Segment>
   );
 }
