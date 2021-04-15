d4df3596499f4088cb58e869ec775d5715b13bcf
Maximilian Eineder <max.eineder@googlemail.com>
2021-04-15 21:59:57 +0200

    Add reusable select input based on Formik

--- client-app/src/app/common/form/MySelectInput.tsx
+++ client-app/src/app/common/form/MySelectInput.tsx
@@ -1,20 +1,28 @@
 import { useField } from "formik";
 import React from "react";
-import { Form, Label } from "semantic-ui-react";
+import { Form, Label, Select } from "semantic-ui-react";
 
 interface Props {
   placeholder: string;
   name: string;
+  options: any;
   label?: string;
 }
 
 function MySelectInput(props: Props) {
-  const [field, meta] = useField(props.name);
+  const [field, meta, helpers] = useField(props.name);
 
   return (
     <Form.Field error={meta.touched && !!meta.error}>
       <label>{props.label}</label>
-      <input {...field} {...props} />
+      <Select
+        clearable
+        options={props.options}
+        value={field.value || null}
+        onChange={(e, d) => helpers.setValue(d.value)}
+        onBlur={() => helpers.setTouched(true)}
+        placeholder={props.placeholder}
+      />
       {meta.touched && meta.error ? (
         <Label basic color="red">
           {meta.error}
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,15 +1,17 @@
-import { observer } from 'mobx-react-lite';
-import { useEffect, useState } from 'react';
-import { useParams } from 'react-router';
-import { Button, Segment } from 'semantic-ui-react';
-import { Form } from 'formik';
-import LoadingComponent from '../../../app/layout/LoadingComponent';
-import { useStore } from '../../../app/stores/Store';
-import { Link } from 'react-router-dom';
-import { Formik } from 'formik';
-import * as Yup from 'yup';
-import MyTextInput from '../../../app/common/form/MyTextInput';
-import MyTextArea from '../../../app/common/form/MyTextArea';
+import { observer } from "mobx-react-lite";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { Button, Segment } from "semantic-ui-react";
+import { Form } from "formik";
+import LoadingComponent from "../../../app/layout/LoadingComponent";
+import { useStore } from "../../../app/stores/Store";
+import { Link } from "react-router-dom";
+import { Formik } from "formik";
+import * as Yup from "yup";
+import MyTextInput from "../../../app/common/form/MyTextInput";
+import MyTextArea from "../../../app/common/form/MyTextArea";
+import MySelectInput from "../../../app/common/form/MySelectInput";
+import { categoryOptions } from "../../../app/common/options/categoryOptions";
 
 export default observer(function ActivityForm() {
   const { activityStore } = useStore();
@@ -18,19 +20,19 @@ export default observer(function ActivityForm() {
   const { id } = useParams<{ id: string }>();
 
   const [activity, setActivity] = useState({
-    id: '',
-    title: '',
-    category: '',
-    description: '',
-    date: '',
-    city: '',
-    venue: '',
+    id: "",
+    title: "",
+    category: "",
+    description: "",
+    date: "",
+    city: "",
+    venue: "",
   });
 
   const validationSchema = Yup.object({
-    title: Yup.string().required('The acitivity title is required.'),
+    title: Yup.string().required("The acitivity title is required."),
     description: Yup.string().required(
-      'The acitivity description is required.'
+      "The acitivity description is required."
     ),
     category: Yup.string().required(),
     date: Yup.string().required(),
@@ -55,7 +57,11 @@ export default observer(function ActivityForm() {
           <Form className="ui form" onSubmit={handleSubmit} autoComplete="off">
             <MyTextInput placeholder="Title" name="title" />
             <MyTextArea placeholder="Description" name="description" rows={3} />
-            <MyTextInput placeholder="Category" name="category" />
+            <MySelectInput
+              options={categoryOptions}
+              placeholder="Category"
+              name="category"
+            />
             <MyTextInput placeholder="Date" name="date" />
             <MyTextInput placeholder="City" name="city" />
             <MyTextInput placeholder="Venue" name="venue" />
