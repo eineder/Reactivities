59d674e9e02b264d4a79b3c78b58627887369366
Max Eineder <max.eineder@googlemail.com>
2021-05-19 22:01:09 +0200

    Remove unused values example classes.

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -10,20 +10,11 @@ namespace Persistence
         {
         }
 
-        public DbSet<Value> Values { get; set; }
-
         public DbSet<Activity> Activities { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
-
-            builder.Entity<Value>()
-                .HasData(
-                    new Value { Id = 1, Name = "101" },
-                    new Value { Id = 2, Name = "102" },
-                    new Value { Id = 3, Name = "103" }
-                );
         }
     }
 }
